cd9d0a19e46e9a6490338727975d250481739e63
Micke Wessén <micke.wessen@hotmail.com>
2020-09-01 11:12:52 +0200

    C2

---
+++ C2_ReadLine/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace C2_ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age = 0;
+
+            Console.Write("Welcome Dude! What's your name? ");
+            name = Console.ReadLine();
+
+            Console.Write($"{name} sweet :) How old are you man? ");
+            //age = Convert.ToInt32(Console.ReadLine());            
+            //age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                age = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch
+            {
+                Console.WriteLine("Sorry dude! Your age must be in digits. Try again man.");
+                Console.Write("How old are you man? ");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            finally
+            {
+                Console.WriteLine($"Oh cool man! We are the samge age. I am also {age}.");
+            }
+                        
+            Console.WriteLine($"Dude your name contains {name.Length} characters.");
+            Console.Write($"{name} where do you live? ");
+            var city = Console.ReadLine();
+
+            Console.WriteLine($"Sweet, my brother lives in {city} aswell");
+
+        }
+    }
+}
