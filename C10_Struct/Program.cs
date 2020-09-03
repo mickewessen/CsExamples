d9fe36cdf8944814a75fbff737ecf29f1a153c5b
Micke Wessén <micke.wessen@hotmail.com>
2020-09-03 10:59:37 +0200

    C11

---
+++ C10_Struct/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace C10_Struct
+{
+    // MINNESHANTERING   (RAM-HDD)
+    // HEAP     = Långsammare   MALOC() FREE()      CLASS
+    // STACK    = Snabbare                          STRUCT
+
+
+    class ClassExample
+    {
+        public int Nr1 { get; set; }
+        public int Nr2 { get; set; }
+
+        public ClassExample(int nr1, int nr2)
+        {
+            Nr1 = nr1;
+            Nr2 = nr2;
+        }
+
+        public void DisplayValues()
+        {
+            Console.WriteLine($"Display Values: {Nr1}, {Nr2}");
+        }
+
+
+        class Program
+        { 
+            private static void ModifyValues(dynamic d)
+            {
+                d.Nr1 += 1;
+                d.Nr1 += 2;
+                Console.WriteLine($"Modyfiying Values: {d.Nr1}, {d.Nr2} ");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Classexample - HEAP (GLOBAL");
+            var classexample = new ClassExample(1, 1);
+
+            classexample.DisplayValues();
+
+            Console.WriteLine("\n------------------------\n");
+
+            Console.WriteLine("Structexample - HEAP (LOCAL");
+            var structexample = new ClassExample(1, 1);
+
+            classexample.DisplayValues();
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ C11_Enum/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace C11_Enum
+{
+    public enum Level
+    {
+        Low,
+        Normal,
+        High,
+    }
+
+    class Program
+    {
+        public enum Status
+        {
+            Pending,
+            Production,
+            Transporting,
+            Delivered,
+        }
+
+        static void Main(string[] args)
+        {
+            var temperature = 32;
+            var _tempLevel = Level.Normal;
+
+            switch (temperature)
+            {
+                case int t when t < 10:
+                    _tempLevel = Level.Low;
+                    break;
+
+                case int t when t > 30:
+                    _tempLevel = Level.High;
+                    break;
+
+                default:
+                    _tempLevel = Level.Normal;
+                    break;
+            }
+
+            switch (_tempLevel)
+            {
+                case Level.Low:
+                    Console.WriteLine("Temperature is to Low");
+                    break;
+                case Level.Normal:
+                    Console.WriteLine("Temperature is to Normal");
+                    break;
+                case Level.High:
+                    Console.WriteLine("Temperature is to High");
+                    break;
+
+            }
+
+            var status = Status.Pending;
+
+            switch (status)
+            {
+                case Status.Pending:
+                    Console.WriteLine("Electronic information has been recieved");
+                    break;
+                case Status.Production:
+                    Console.WriteLine("Your product(s) are being prepared for shipping");
+                    break;
+                case Status.Transporting:
+                    Console.WriteLine("Your product(s) are in transit");
+                    break;
+                case Status.Delivered:
+                    Console.WriteLine("Your product(s) has been delivered");
+                    break;
+            }
+
+            Console.WriteLine(_tempLevel);
+
+        }
+    }
+}
