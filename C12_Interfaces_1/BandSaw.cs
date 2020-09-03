08c6dd6bab1fc0bbd2a957c3ad7a175fbd586a82
Micke Wessén <micke.wessen@hotmail.com>
2020-09-03 11:36:49 +0200

    C12

---
+++ C12_Interfaces_1/BandSaw.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C12_Interfaces_1
+{
+    class BandSaw : IMachineFeatures
+    {
+        public bool Start()
+        {
+            Console.WriteLine("BandSaw Machine has started.");
+            return true;
+        }
+
+        public bool Stop()
+        {
+            Console.WriteLine("Bandsaw Machine has stopped.");
+            return true;
+        }
+    }
+}
